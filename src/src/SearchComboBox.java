aad724cc842bc02d2f0e890d514a5a79ecd65c9e
dongjianbin <dongjianbin2001@gmail.com>
2014-11-08 11:28:20 +0800

    modify
    modify

--- src/src/SearchComboBox.java
+++ src/src/SearchComboBox.java
@@ -1,10 +1,13 @@
 package src;
 
-
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import java.util.List;
 import java.util.Vector;
 
@@ -21,248 +24,276 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.JOptionPane;
 import javax.swing.text.Document;
+import java.awt.Robot;
+
 
 /**
- * @author SamZheng
- * 带有自动检查功能的CombBox
+ * @author SamZheng 带有自动检查功能的CombBox
  */
-public class SearchComboBox
-    extends JComboBox {
-  private AutoCompleter completer;
-  private Vector orilist;
-  
-  public void setOrilist(Vector v){
-	  orilist=v;
-  }
-  public Vector  getOrilist(){
-	  return this.orilist;
-	  
-  }
-  
+public class SearchComboBox extends JComboBox {
+	private AutoCompleter completer;
+	private Vector orilist;
 
-  public SearchComboBox() {
-    super();
-    addCompleter();
-  }
+	public void setOrilist(Vector v) {
+		orilist = v;
+	}
 
-  public SearchComboBox(ComboBoxModel cm) {
-    super(cm);
-    setEditable(true);
-    addCompleter();
-  }
-  public SearchComboBox(DefaultComboBoxModel dcm,Vector v) {
-	    super(dcm);
-	    this.orilist=v;
-	    setEditable(true);
-	    addCompleter();
-	  }
+	public Vector getOrilist() {
+		return this.orilist;
 
-  public SearchComboBox(Object[] items) {
-    super(items);
-    addCompleter();
-  }
+	}
 
-  public SearchComboBox(List v) {
-    super( (Vector) v);
-    addCompleter();
-  }
+	public SearchComboBox() {
+		super();
+		addCompleter();
+	}
 
-  private void addCompleter() {
-    setEditable(true);
-    completer = new AutoCompleter(this);
-  }
+	public SearchComboBox(ComboBoxModel cm) {
+		super(cm);
+		setEditable(true);
+		addCompleter();
+	}
 
-  public void autoComplete(String str) {
-    this.completer.autoComplete(str, str.length());
-  }
+	public SearchComboBox(DefaultComboBoxModel dcm, Vector v) {
+		super(dcm);
+		this.orilist = v;
+		setEditable(true);
+		addCompleter();
+	}
 
-  public String getText() {
-    return ( (JTextField) getEditor().getEditorComponent()).getText();
-  }
+	public SearchComboBox(Object[] items) {
+		super(items);
+		addCompleter();
+	}
 
-  public void setText(String text) {
-    ( (JTextField) getEditor().getEditorComponent()).setText(text);
-  }
+	public SearchComboBox(List v) {
+		super((Vector) v);
+		addCompleter();
+	}
 
-  public boolean containsItem(String itemString) {
-    for (int i = 0; i < this.getModel().getSize(); i++) {
-      String _item = "" + this.getModel().getElementAt(i);
-      if (_item.equals(itemString)) {
-        return true;
-      }
-    }
-    return false;
-  }
+	private void addCompleter() {
+		setEditable(true);
+		completer = new AutoCompleter(this);
+	}
 
-  /*
-   * 测试方法
-   */
-  public static void main(String[] args) {
-    JFrame frame = new JFrame();
-    Object[] items = new Object[] {
-        "zzz","zba","aab","abc", "aab","dfg","aba", "hpp", "pp", "hlp"};
-    
+	public void autoComplete(String str) {
+		this.completer.autoComplete(str, str.length());
+	}
 
-	Vector labels2= new Vector();
-	labels2.add("aaa");
-	labels2.add("bcd");
-	labels2.add("efg");
-	labels2.add("sda");
-	labels2.add("adef");
-	DefaultComboBoxModel mDefaultComboBoxModel=new DefaultComboBoxModel(labels2);
-    //排序内容
-    //java.util.ArrayList list = new java.util.ArrayList(Arrays.asList(items));
-    //Collections.sort(list);
-    //JComboBox cmb = new JAutoCompleteComboBox(list.toArray());
-    Arrays.sort(items);
-    
-    JComboBox cmb = new SearchComboBox(mDefaultComboBoxModel,labels2);
-    cmb.setSelectedIndex(-1);
-    frame.getContentPane().add(cmb);
-    frame.setSize(400, 80);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
+	public String getText() {
+		return ((JTextField) getEditor().getEditorComponent()).getText();
+	}
+
+	public void setText(String text) {
+		((JTextField) getEditor().getEditorComponent()).setText(text);
+	}
+
+	public boolean containsItem(String itemString) {
+		for (int i = 0; i < this.getModel().getSize(); i++) {
+			String _item = "" + this.getModel().getElementAt(i);
+			if (_item.equals(itemString)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * 测试方法
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		Object[] items = new Object[] { "zzz", "zba", "aab", "abc", "aab",
+				"dfg", "aba", "hpp", "pp", "hlp" };
+
+		Vector labels1 = new Vector();
+		Vector labels2 = new Vector();
+		labels2.add("aaa");
+		labels2.add("bcd");
+		labels2.add("efg");
+		labels2.add("sda");
+		labels2.add("adef");
+		DefaultComboBoxModel mDefaultComboBoxModel = new DefaultComboBoxModel();
+		// 排序内容
+		// java.util.ArrayList list = new
+		// java.util.ArrayList(Arrays.asList(items));
+		// Collections.sort(list);
+		// JComboBox cmb = new JAutoCompleteComboBox(list.toArray());
+		Arrays.sort(items);
+
+		JComboBox cmb = new SearchComboBox(mDefaultComboBoxModel, labels2);
+		cmb.setSelectedIndex(-1);
+		frame.getContentPane().add(cmb);
+		frame.setSize(400, 80);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
 
 /**
- *   自动完成器。自动找到最匹配的项目，并排在列表的最前面。
- *   @author   SamZheng
+ * 自动完成器。自动找到最匹配的项目，并排在列表的最前面。
+ * 
+ * @author Justin Dong
  */
 
-class AutoCompleter
-    implements KeyListener, ItemListener {
+class AutoCompleter implements KeyListener, ItemListener {
 
-  private JComboBox owner = null;
-  private JTextField editor = null;
+	private JComboBox owner = null;
+	private JTextField editor = null;
 
-  private ComboBoxModel model = null;
-  private Vector orilist = null; 
-  private Document mDocument=null;
+	private ComboBoxModel model = null;
+	private Vector orilist = null;
+	private Document mDocument = null;
+	private Robot robot;
 
-  public AutoCompleter(SearchComboBox comboBox) {
-    owner = comboBox;
-    orilist = ((SearchComboBox) owner).getOrilist();
-    System.out.println(orilist.toString());
-    editor = (JTextField) comboBox.getEditor().getEditorComponent();
-    mDocument=editor.getDocument();
-    
-    editor.addKeyListener(this);
-    model = comboBox.getModel();
-    owner.addItemListener(this);
-    
-    
-  }
+	public AutoCompleter(SearchComboBox comboBox) {
+		owner = comboBox;
+		orilist = ((SearchComboBox) owner).getOrilist();
+		System.out.println(orilist.toString());
+		editor = (JTextField) comboBox.getEditor().getEditorComponent();
+		mDocument = editor.getDocument();
+		mDocument.addDocumentListener(new DocumentListener() {
+			public void insertUpdate(DocumentEvent e) {
+				System.out.println("insertUpdate");
+				String m = editor.getText();
+				System.out.println("Input String is : " + m);
 
-  public void keyTyped(KeyEvent e) {}
 
-  public void keyPressed(KeyEvent e) {}
+			}
 
-  public void keyReleased(KeyEvent e) {
-    char ch = e.getKeyChar();
-    if (ch == KeyEvent.CHAR_UNDEFINED || Character.isISOControl(ch) ||
-        ch == KeyEvent.VK_DELETE) {
-      return;
-    }
+			public void removeUpdate(DocumentEvent e) {
+				System.out.println("removeUpdate");
+			}
 
-    int caretPosition = editor.getCaretPosition();
-    String str = editor.getText();
-    System.out.println("Input strings : " + str +"position :" + caretPosition);
-    if (str.length() == 0) {
-      return;
-    }
-//    autoComplete(str, caretPosition);
-    autoComplete(str);
-  }
+			public void changedUpdate(DocumentEvent e) {
+				System.out.println("changedUpdate");
+			}
+		});
 
-  protected void autoComplete(String strf) {
-	  Vector opts;
-	  opts = getMatchingVector(strf);
-	    if (owner != null) {
-	        model = new DefaultComboBoxModel(opts);
-	        owner.setModel(model);
-	        editor.setText(strf);
-//	        owner.setSelectedIndex(-1);
-	      }
-	    if (opts.size() > 0) {
-	        String str = opts.get(0).toString();
-	        if (owner != null) {
-	          try {
-	            owner.showPopup();
-	          }
-	          catch (Exception ex) {
-	            ex.printStackTrace();
-	          }
-	        }
-	      }
-  }
-  
-  /**
-   *   自动完成。根据输入的内容，在列表中找到相似的项目.
-   */
-  protected void autoComplete(String strf, int caretPosition) {
+		editor.addKeyListener(this);
+		model = comboBox.getModel();
+		owner.addItemListener(this);
+
+	}
+
+	public void DocumentListener(){
+		
+		
+	}
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public void keyPressed(KeyEvent e) {
+	}
+
+	public void keyReleased(KeyEvent e) {
+		char ch = e.getKeyChar();
+		System.out.println("char is "+ ch);
+//		if (ch == KeyEvent.CHAR_UNDEFINED || Character.isISOControl(ch)
+//				|| ch == KeyEvent.VK_DELETE) {
+//			return;
+//		}
+		if (ch == KeyEvent.CHAR_UNDEFINED ) {
+			System.out.println("char undefined");
+			return;
+		}
+
+		int caretPosition = editor.getCaretPosition();
+		String str = editor.getText();
+		System.out.println("Input strings : " + str + "position :"
+				+ caretPosition);
+		if (str.length() == 0) {
+			return;
+		}
+		// autoComplete(str, caretPosition);
+		autoComplete(str);
+	}
+
+	protected void autoComplete(String strf) {
+		Vector opts;
+		opts = getMatchingVector(strf);
+		if (owner != null) {
+			model = new DefaultComboBoxModel(opts);
+			owner.setModel(model);
+			editor.setText(strf);
+			// owner.setSelectedIndex(-1);
+		}
+		if (opts.size() > 0) {
+			String str = opts.get(0).toString();
+			if (owner != null) {
+				try {
+					owner.showPopup();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * 自动完成。根据输入的内容，在列表中找到相似的项目.
+	 */
+	protected void autoComplete(String strf, int caretPosition) {
 
 		System.out.println("autoComplete here");
-    Object[] opts;
-    opts = getMatchingOptions(strf.substring(0, caretPosition));
-    if (owner != null) {
-      model = new DefaultComboBoxModel(opts);
-      owner.setModel(model);
-    }
-    if (opts.length > 0) {
-      String str = opts[0].toString();
-      if(caretPosition>editor.getText().length()) return;
-      editor.setCaretPosition(caretPosition);
-      editor.setText(editor.getText().trim().substring(0,caretPosition));
-      if (owner != null) {
-        try {
-          owner.showPopup();
-        }
-        catch (Exception ex) {
-          ex.printStackTrace();
-        }
-      }
-    }
-  }
+		Object[] opts;
+		opts = getMatchingOptions(strf.substring(0, caretPosition));
+		if (owner != null) {
+			model = new DefaultComboBoxModel(opts);
+			owner.setModel(model);
+		}
+		if (opts.length > 0) {
+			String str = opts[0].toString();
+			if (caretPosition > editor.getText().length())
+				return;
+			editor.setCaretPosition(caretPosition);
+			editor.setText(editor.getText().trim().substring(0, caretPosition));
+			if (owner != null) {
+				try {
+					owner.showPopup();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
 
-  /**
-   *
-   *   找到相似的项目,   并且将之排列到数组的最前面。
-   *   @param   str
-   *   @return   返回所有项目的列表。
-   */
-  protected Object[] getMatchingOptions(String str) {
-    List v = new Vector();
-    List v1 = new Vector();
+	/**
+	 *
+	 * 找到相似的项目, 并且将之排列到数组的最前面。
+	 * 
+	 * @param str
+	 * @return 返回所有项目的列表。
+	 */
+	protected Object[] getMatchingOptions(String str) {
+		List v = new Vector();
+		List v1 = new Vector();
 
-    for (int k = 0; k < model.getSize(); k++) {
-      Object itemObj = model.getElementAt(k);
-      if (itemObj != null) {
-        String item = itemObj.toString().toLowerCase();
-        if (item.startsWith(str.toLowerCase())) {
-          v.add(model.getElementAt(k));
-        }
-        else {
-          v1.add(model.getElementAt(k));
-        }
-      }
-      else {
-        v1.add(model.getElementAt(k));
-      }
-    }
-    for (int i = 0; i < v1.size(); i++) {
-      v.add(v1.get(i));
-    }
-    if (v.isEmpty()) {
-      v.add(str);
-    }
-    return v.toArray();
-  }
+		for (int k = 0; k < model.getSize(); k++) {
+			Object itemObj = model.getElementAt(k);
+			if (itemObj != null) {
+				String item = itemObj.toString().toLowerCase();
+				if (item.startsWith(str.toLowerCase())) {
+					v.add(model.getElementAt(k));
+				} else {
+					v1.add(model.getElementAt(k));
+				}
+			} else {
+				v1.add(model.getElementAt(k));
+			}
+		}
+		for (int i = 0; i < v1.size(); i++) {
+			v.add(v1.get(i));
+		}
+		if (v.isEmpty()) {
+			v.add(str);
+		}
+		return v.toArray();
+	}
 
-
-  protected Vector getMatchingVector(String str) {
-	  Vector v = new Vector();
-	  Vector v1 = new Vector();
+	protected Vector getMatchingVector(String str) {
+		Vector v = new Vector();
+		Vector v1 = new Vector();
 
 		if (str.length() != 0) {
 			System.out.println("Start");
@@ -276,26 +307,24 @@ class AutoCompleter
 				}
 			}
 		}
-    return v;
-  }
-  
-  public void itemStateChanged(ItemEvent event) {
-	  System.out.println("aaa");
-    if (event.getStateChange() == ItemEvent.SELECTED) {
+		return v;
+	}
 
-  	  System.out.println("bbb");
-      int caretPosition = editor.getCaretPosition();
-      if (caretPosition != -1) {
-        try {
+	public void itemStateChanged(ItemEvent event) {
+		System.out.println("aaa");
+		if (event.getStateChange() == ItemEvent.SELECTED) {
 
-        	  System.out.println("ccc");
-//          editor.moveCaretPosition(caretPosition);
-        }
-        catch (IllegalArgumentException ex) {
-          ex.printStackTrace();
-        }
-      }
-    }
-  }
+			System.out.println("bbb");
+			int caretPosition = editor.getCaretPosition();
+			if (caretPosition != -1) {
+				try {
+
+					System.out.println("ccc");
+					// editor.moveCaretPosition(caretPosition);
+				} catch (IllegalArgumentException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
 }
- 
